c82a37702b0a979e3026454d46d3775c0ee648c9
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-08-09 16:18:07 +0200

    persist checked status a  todo

--- script.js
+++ script.js
@@ -4,25 +4,47 @@ const form = document.querySelector("#new-todo-form");
 const formInput = document.querySelector("#todo-input");
 const list = document.querySelector("#list");
 const template = document.querySelector("#list-item-template");
-const todos = [];
-const TODO_STORAGE_KEY = "ADVANCED-TODO-LIST-";
+const TODO_STORAGE_KEY = "ADVANCED-TODO-LIST-todos";
+const todos = loadTodos();
+
+todos.forEach((todo) => renderTodo(todo));
+// todos.forEach(renderTodo); // This works too
+
+list.addEventListener("change", (e) => {
+  if (!e.target.matches("[data-list-item-checkbox]")) return;
+  const parent = e.target.closest(".list-item");
+  const todoId = parent.dataset.todoId;
+  const todo = todos.find((todo) => todo.id === todoId);
+  todo.complete = e.target.checked;
+  saveTodos();
+  console.log(todo);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const todoName = formInput.value;
   if (todoName === "") return;
-  todos.push(todoName);
-  renderTodo(todoName);
+  const newTodo = {
+    name: todoName,
+    complete: false,
+    id: new Date().valueOf().toString(),
+  };
+  todos.push(newTodo);
+  renderTodo(newTodo);
   saveTodos();
   formInput.value = "";
 });
 
 // render Todo
-function renderTodo(todoName) {
+function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true);
+  const listItem = templateClone.querySelector(".list-item");
+  listItem.dataset.todoId = todo.id;
   const textElement = templateClone.querySelector("[data-list-item-text]");
-  textElement.innerText = todoName;
+  textElement.innerText = todo.name;
+  const checkBox = templateClone.querySelector("[data-list-item-checkbox]");
+  checkBox.checked = todo.complete;
   list.appendChild(templateClone);
 }
 
@@ -30,3 +52,11 @@ function renderTodo(todoName) {
 function saveTodos() {
   localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(todos));
 }
+
+// Load Todos
+function loadTodos() {
+  const todosString = localStorage.getItem(TODO_STORAGE_KEY);
+  return JSON.parse(todosString) || [];
+}
+
+// complete Todos
